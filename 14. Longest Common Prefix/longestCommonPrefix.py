0805df8909113c6e75f64a99b4054998a14f34dc
mustangLIN <mustang.x.lin@gmail.com>
2018-11-08 21:25:42 +0800

    add Longest Common Prefix

---
+++ 14. Longest Common Prefix/longestCommonPrefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if strs == None:
+            return ""
+        
+        if len(strs) == 0:
+            return ""
+
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or c != strs[j][i]:
+                    return strs[j][0:i]
