9988a74ba421617c99a4aa2181c3d012137dcb21
mustangLIN <mustang.x.lin@gmail.com>
2018-11-08 21:34:14 +0800

    fix

--- 14. Longest Common Prefix/longestCommonPrefix.py
+++ 14. Longest Common Prefix/longestCommonPrefix.py
@@ -4,10 +4,7 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        if strs == None:
-            return ""
-        
-        if len(strs) == 0:
+        if strs == None or len(strs) == 0:
             return ""
 
         for i in range(len(strs[0])):
@@ -15,3 +12,5 @@ class Solution:
             for j in range(1,len(strs)):
                 if i == len(strs[j]) or c != strs[j][i]:
                     return strs[j][0:i]
+        
+        return strs[0]
