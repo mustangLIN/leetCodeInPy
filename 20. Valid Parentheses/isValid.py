fd177fd36962ae665a20adeadad2ffba15a284a9
mustangLIN <mustang.x.lin@gmail.com>
2018-11-11 08:40:50 +0800

    optimization

--- 20. Valid Parentheses/isValid.py
+++ 20. Valid Parentheses/isValid.py
@@ -4,27 +4,28 @@ class Solution:
         :type s: str
         :rtype: bool
         """
+        if not s:
+            return True
+        if len(s) == 1:
+            return False
+        
         dict = {
             "(" : ")",
             "[" : "]",
             "{" : "}"
         }
+
         stack = []
         for c in s:
-            # check c is right Parentheses 
-            if c in dict.values():
-                # c is right Parentheses, stack is empty, no pair
-                if not stack:
-                    return False
-                
-                if dict[c] == stack[-1]:
-                    del stack[-1]
-                else:
-                    return False
-
-            # c is left, put in stack, wait to compare
-            else:
+            # check c is left Parentheses, put in stack, wait couple
+            if c in dict.keys():
                 stack.append(c)
+            # c is right, couple couple
+            elif len(stack) > 0 and c == dict[stack[-1]]:
+                    del stack[-1]
+            # c is right, stack is empty nothing to couple, or couple fail
+            else:
+                return False
 
         # stack == empty, True or False
         return not stack
