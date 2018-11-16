dc0d9222c3bbe87034d49e31ec941964c75ea759
mustangLIN <mustang.x.lin@gmail.com>
2018-11-16 09:47:06 +0800

    add 27. Remove Element

---
+++ 27. Remove Element/removeElement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if not nums or len(nums) == 0:
+            return 0
+        
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        
+        return count
